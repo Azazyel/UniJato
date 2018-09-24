9db84f425e80ecc874c2ba14182177df99161947
Lucas Pereira <Lucas Pereira@172.16.90.226>
2018-09-24 16:23:49 -0300

    versão: a0.1

---
+++ src/br/unitins/unijato/controller/LoginController.java
@@ -0,0 +1,12 @@
+package br.unitins.unijato.controller;
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+
+@Named
+public class LoginController implements Serializable {
+
+	private static final long serialVersionUID = -1865949396726049923L;
+
+}
