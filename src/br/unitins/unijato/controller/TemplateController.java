1ddeab589fa3c28495255a370f0745ebe8bcd51a
iury <iury@iury-PC>
2018-12-12 07:01:44 -0300

    99p finalizado part2

---
+++ src/br/unitins/unijato/controller/TemplateController.java
@@ -0,0 +1,14 @@
+package br.unitins.unijato.controller;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class TemplateController extends DefaultController {
+
+	private static final long serialVersionUID = 3315885899410185656L;
+
+	
+	
+}
